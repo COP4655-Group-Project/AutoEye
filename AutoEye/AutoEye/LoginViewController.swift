8a1e2b6ee85afc567a39b1eeaf6f924c838a5961
maggielafratta23 <122281308+maggielafratta23@users.noreply.github.com>
2023-03-24 14:32:25 -0400

    Login Page
    Added LoginViewController, need to make file for this & registration.

---
+++ AutoEye/AutoEye/LoginViewController.swift
@@ -0,0 +1,59 @@
+//
+//  LoginViewController.swift
+//  AutoEye
+//
+//  Created by Maggie LaFratta on 3/24/23.
+//
+
+import Foundation
+import UIKit
+
+
+//**need to download packages from online I think, not available locally
+//import ParseSwift
+
+class LoginViewController: UIViewController {
+
+        //****Need to make login page in storyboard for IBOutlets
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func onLoginTapped(_ sender: Any) {
+
+        // Make sure all fields are non-nil and non-empty.
+        guard let username = usernameField.text,
+              let password = passwordField.text,
+              !username.isEmpty,
+              !password.isEmpty else {
+
+            showMissingFieldsAlert()
+            return
+        }
+
+        User.login(username: username, password: password) { [weak self] result in
+
+            switch result {
+            case .success(let user):
+                print("✅ Successfully logged in as user: \(user)")
+
+                // Post a notification that the user has successfully logged in.
+                NotificationCenter.default.post(name: Notification.Name("login"), object: nil)
+
+            case .failure(let error):
+                // Show an alert for any errors
+                self?.showAlert(description: error.localizedDescription)
+            }
+        }
+    }
+
+    private func showMissingFieldsAlert() {
+        let alertController = UIAlertController(title: "Opps...", message: "We need all fields filled out in order to log you in.", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(action)
+        present(alertController, animated: true)
+    }
+}
