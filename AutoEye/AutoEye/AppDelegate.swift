8c8d749ed200f0ca244e4695177b974ffd64eea8
maggielafratta23 <122281308+maggielafratta23@users.noreply.github.com>
2023-03-26 14:55:26 -0400

    Updating login/sign up

--- AutoEye/AutoEye/AppDelegate.swift
+++ AutoEye/AutoEye/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+//***need to install package here
+//import parseSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +16,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        ParseSwift.initialize(applicationId: "FAJRXteEnYzXSW3j1JSjetMah2nwdaBzlweZVV5e",
+                              clientKey: "n9QrXm9kXW4zxQg8X6q5wt9kLIdSq6h1m1QElV4x",
+                              serverURL: URL(string: "https://parseapi.back4app.com")!)
+        // Testing back4app stuff
+        var score = GameScore()
+        score.playerName = "Kingsley"
+        score.points = 13
+
+        // Save to your server asynchronously (preferred way) - Performs work on background queue and returns to specified callbackQueue.
+        // If no callbackQueue is specified it returns to main queue.
+        score.save { result in
+            switch result {
+            case .success(let savedScore):
+                print("✅ Parse Object SAVED!: Player: \(String(describing: savedScore.playerName)), Score: \(String(describing: savedScore.points))")
+            case .failure(let error):
+                assertionFailure("Error saving: \(error)")
+            }
+        }
         return true
     }
 
@@ -33,4 +53,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
+
+// Test to make sure our database works
+struct GameScore: ParseObject {
+    // required by ParseObject
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseACL?
+    var originalData: Data?
+
+    var playerName: String?
+    var points: Int?
+}
+
+// Sample Usage
+//
+// var score = GameScore()
+// score.playerName = "Kingsley"
+// score.points = 13
+
+// OR Implement a custom initializer (OPTIONAL i.e. NOT REQUIRED)
+// It's recommended to place custom initializers in an extension
+// to preserve the memberwise initializer.
+extension GameScore {
+
+    // Use the init to set your custom properties
+    // NOTE: Properties in custom init are NOT required to be optional
+    init(playerName: String, points: Int) {
+        self.playerName = playerName
+        self.points = points
+    }
+}
+
+// Sample Usage
+//
+// let score = GameScore(playerName: "Kingsley", points: 13)
 
---
+++ AutoEye/AutoEye/Login/SignUpViewController.swift
@@ -0,0 +1,68 @@
+//
+//  SignUpViewController.swift
+//  AutoEye
+//
+//  Created by Maggie LaFratta on 3/24/23.
+//
+
+import Foundation
+
+import UIKit
+//***reiterating package import as self note
+//import ParseSwift
+
+class SignUpViewController: UIViewController {
+    //need to connect these in storyboard
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func onSignUpTapped(_ sender: Any) {
+
+        // Make sure all fields are non-nil and non-empty.
+        guard let username = usernameField.text,
+              let email = emailField.text,
+              let password = passwordField.text,
+              !username.isEmpty,
+              !email.isEmpty,
+              !password.isEmpty else {
+
+            showMissingFieldsAlert()
+            return
+        }
+
+        var newUser = User()
+        newUser.username = username
+        newUser.email = email
+        newUser.password = password
+
+        newUser.signup { [weak self] result in
+
+            switch result {
+            case .success(let user):
+
+                print("✅ Successfully signed up user \(user)")
+
+                // Post a notification that the user has successfully signed up.
+                NotificationCenter.default.post(name: Notification.Name("login"), object: nil)
+
+            case .failure(let error):
+                // Failed sign up
+                self?.showAlert(description: error.localizedDescription)
+            }
+        }
+    }
+
+    private func showMissingFieldsAlert() {
+        let alertController = UIAlertController(title: "Opps...", message: "We need all fields filled out in order to sign you up.", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(action)
+        present(alertController, animated: true)
+    }
+}
+
