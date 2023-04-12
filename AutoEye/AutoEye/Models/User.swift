62ea48a92e16a86ad1b97130835e885c751eab09
maggielafratta23 <122281308+maggielafratta23@users.noreply.github.com>
2023-04-12 12:44:36 -0400

    Model folder User file

---
+++ AutoEye/AutoEye/Models/User.swift
@@ -0,0 +1,32 @@
+//
+//  User.swift
+//  AutoEye
+//
+//  Created by Maggie LaFratta on 4/12/23.
+//
+
+import Foundation
+import ParseSwift
+
+struct User: ParseUser {
+    // required by `ParseObject`
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseACL?
+    var originalData: Data?
+
+    // required by `ParseUser`
+    var username: String?
+    var email: String?
+    var emailVerified: Bool?
+    var password: String?
+    var authData: [String: [String: String]?]?
+
+    // Your custom properties.
+    
+    var licensePlate: String?
+
+    // TODO: Pt 2 - Add custom property for `lastPostedDate`
+
+}
