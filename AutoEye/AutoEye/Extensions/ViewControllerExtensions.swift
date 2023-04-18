2ee6702c0d6009a132c517e777bb71a30a6e1cb8
harshinioruganti <122567498+harshinioruganti@users.noreply.github.com>
2023-04-18 00:26:14 -0400

    Revert "Stash changes"
    This reverts commit e8344eab2e687fcd489acbfe8d1b703189329e73.

--- AutoEye/AutoEye/Extensions/ViewControllerExtensions.swift
+++ AutoEye/AutoEye/Extensions/ViewControllerExtensions.swift
@@ -5,15 +5,4 @@
 //  Created by Maggie LaFratta on 4/12/23.
 //
 
-import UIKit
-
-extension UIViewController {
-
-    // Shows an alert with the given description or default text if nil.
-    func showAlert(description: String? = nil) {
-        let alertController = UIAlertController(title: "Oops...", message: "\(description ?? "Unknown error")", preferredStyle: .alert)
-        let action = UIAlertAction(title: "OK", style: .default)
-        alertController.addAction(action)
-        present(alertController, animated: true)
-    }
-}
+import Foundation
