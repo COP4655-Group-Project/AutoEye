e8344eab2e687fcd489acbfe8d1b703189329e73
harshinioruganti <122567498+harshinioruganti@users.noreply.github.com>
2023-04-18 00:14:13 -0400

    Stash changes

--- AutoEye/AutoEye/Extensions/ViewControllerExtensions.swift
+++ AutoEye/AutoEye/Extensions/ViewControllerExtensions.swift
@@ -5,4 +5,15 @@
 //  Created by Maggie LaFratta on 4/12/23.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+
+    // Shows an alert with the given description or default text if nil.
+    func showAlert(description: String? = nil) {
+        let alertController = UIAlertController(title: "Oops...", message: "\(description ?? "Unknown error")", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(action)
+        present(alertController, animated: true)
+    }
+}
